8eb3fb0e885beab19d5eccc83196939acdb2b688
Jordan Stormo <jordanstormo@yahoo.com>
2015-07-08 18:28:51 -0500

    Update Project 2.cpp

--- Project 2.cpp
+++ Project 2.cpp
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 using namespace std;
 
-void show(int [],int);
-void creatures(int [], int, int, int, int, int);
-void move(int[],int,int,int,int,int);
+void show(int[], int);
+void creatures(int[], int, int, int, int, int);
+void move(int[], int, int, int, int, int);
 void gameover();
 
 void main()
@@ -14,15 +14,15 @@ void main()
 	srand(time(0));
 	const int N = 10;
 	int road[N];
-	int hero_pos=0;
-	int one=0;
-	int two=0;
-	int three=0;
+	int hero_pos = 0;
+	int one = 0;
+	int two = 0;
+	int three = 0;
 
 	for (int i = 0; i < N; i++) road[i] = 0;
 
 	show(road, N);
-	creatures(road, N, hero_pos, one, two,three);
+	creatures(road, N, hero_pos, one, two, three);
 	move(road, N, hero_pos, one, two, three);
 }
 
@@ -35,10 +35,10 @@ void show(int road[], int N)
 	cout << endl;
 }
 
-void creatures(int road[], int N,int hero_pos, int one, int two, int three)
+void creatures(int road[], int N, int hero_pos, int one, int two, int three)
 {
 	hero_pos = 0;
-	one= rand() % 9 + 1;
+	one = rand() % 9 + 1;
 	two = rand() % 9 + 1;
 	three = rand() % 9 + 1;
 
@@ -49,7 +49,7 @@ void creatures(int road[], int N,int hero_pos, int one, int two, int three)
 		one = rand() % 9 + 1;
 		two = rand() % 9 + 1;
 		three = rand() % 9 + 1;
-	} while (one == two || one == three || two==three);
+	} while (one == two || one == three || two == three);
 
 	int numbers[3] = { one, two, three };
 	int temp;
@@ -66,9 +66,9 @@ void creatures(int road[], int N,int hero_pos, int one, int two, int three)
 			}
 		}
 	}
-		
 
-	road[numbers[0]]=3;
+
+	road[numbers[0]] = 3;
 	road[numbers[1]] = 7;
 	road[numbers[2]] = 5;
 
@@ -76,21 +76,23 @@ void creatures(int road[], int N,int hero_pos, int one, int two, int three)
 	{
 		cout << road[i];
 	}
-	
+
 }
 
-void move(int road[], int N, int hero_pos,int one, int two, int three)
+void move(int road[], int N, int hero_pos, int one, int two, int three)
 {
 	char answer;
 	int monsterkilled = 0;
 	int heropow = 10;
+	int monster;
+	int hero;
 
 
 	for (int i = 0; hero_pos != 9; i++)
 	{
 		cout << "Do you want to move: Left(l), Right(r), or Quit(q) the game?" << endl;
 		cin >> answer;
-		if (answer == 'L' || answer== 'l')
+		if (answer == 'L' || answer == 'l')
 		{
 			if (hero_pos == 0)
 			{
@@ -98,7 +100,7 @@ void move(int road[], int N, int hero_pos,int one, int two, int three)
 			}
 			else hero_pos -= 1;
 		}
-		else if (answer == 'R' || answer=='r')
+		else if (answer == 'R' || answer == 'r')
 		{
 			if (hero_pos == 9)
 			{
@@ -107,65 +109,102 @@ void move(int road[], int N, int hero_pos,int one, int two, int three)
 			if (hero_pos == one || hero_pos == two || hero_pos == three)
 			{
 				cout << "You have come across a Monster!" << endl;
-				
+
 				if (hero_pos == one)
 				{
-					for (int i = 0; heropow != 0 || heropow < monster; i++){
-					hero = rand() % heropow;
-					monster = rand() % 3;
+					for (int i = 0; heropow != 0 || hero < monster; i++){
+						hero = rand() % heropow;
+						monster = rand() % 3;
 
-					cout << "Hero rolled a " << hero << " Monster rolled a " << monster;
-					if (heropow < monster)
+						cout << "Hero rolled a " << hero << " Monster rolled a " << monster;
+						while (hero == monster)
+						{
+							hero = rand() % heropow;
+							monster = rand() % 3;
+
+							cout << "Hero rolled a " << hero << " Monster rolled a " << monster;
+						}
+						if (hero < monster)
+						{
+							cout << "You LOST" << endl;
+							hero = monster - hero;
+							heropow -= hero;
+						}
+					}
+					if (hero > monster)
 					{
-						cout << "You LOST" << endl;
-						hero = monster - hero;
-						heropow -= hero;
+						cout << "You Won!" << endl;
+						monsterkilled++;
+					}
+					}
+					if (hero_pos == two)
+					{
+						hero = rand() % heropow;
+						monster = rand() % 5;
+
+						cout << "Hero rolled a " << heropow << " Monster rolled a " << monster;
+						while (hero == monster)
+						{
+							hero = rand() % heropow;
+							monster = rand() % 5;
+
+							cout << "Hero rolled a " << hero << " Monster rolled a " << monster;
+						}
+						if (heropow < monster)
+						{
+							cout << "You LOST" << endl;
+								heropow -= monster;
+							hero_pos = heropow;
+						}
+						if (hero > monster)
+						{
+							cout << "You Won!" << endl;
+							monsterkilled++;
+						}
+					}
+					if (hero_pos == three)
+					{
+						hero = rand() % heropow;
+						monster = rand() % 7;
+
+						cout << "Hero rolled a " << heropow << " Monster rolled a " << monster;
+						while (hero == monster)
+						{
+							hero = rand() % heropow;
+							monster = rand() % 7;
+
+							cout << "Hero rolled a " << hero << " Monster rolled a " << monster;
+						}
+						if (heropow < monster)
+						{
+							cout << "You LOST" << endl;
+								heropow -= monster;
+							hero_pos = heropow;
+						}
+						if (hero > monster)
+						{
+							cout << "You Won!" << endl;
+							monsterkilled++;
+						}
 					}
 				}
-				if (hero_pos == two)
-				{
-					hero = rand() % heropow;
-					monster = rand() % 5;
-
-					cout << "Hero rolled a " << heropow << " Monster rolled a " << monster;
-					if (heropow < monster)
-					{
-						cout << "You LOST" << endl
-						heropow -= monster;
-						hero_pos = heropow;
-					}
-				}
-				if (hero_pos == three)
-				{
-					hero = rand() % heropow;
-					monster = rand() % 7;
-
-					cout << "Hero rolled a " << heropow << " Monster rolled a " << monster;
-					if (heropow < monster)
-					{
-						cout << "You LOST" << endl
-						heropow -= monster;
-						hero_pos = heropow;
-					}
-				}
+				else hero_pos += 1;
 			}
-			else hero_pos += 1;
-		}
-		else if (answer == 'q' || answer == 'Q')
-		{
-			exit(0);
-		}
-		
-		if (heropow == 0)
-		{
-			cout << "Your strength has ran out, warrior! Game over"<<endl;
-			exit(0);
-		}
-		
-		if (monsterskilled == 3)
-		{
-			cout << "You have killed all the monsters! Good job" << endl;
-			exit(0);
+			else if (answer == 'q' || answer == 'Q')
+			{
+				exit(0);
+			}
+
+			if (heropow == 0)
+			{
+				cout << "Your strength has ran out, warrior! Game over" << endl;
+				exit(0);
+			}
+
+			if (monsterkilled == 3)
+			{
+				cout << "You have killed all the monsters! Good job" << endl;
+				exit(0);
+			}
 		}
 	}
-}
